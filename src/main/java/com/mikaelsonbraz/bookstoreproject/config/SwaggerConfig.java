c192ac144cb38102b9d206960970cce2a056801a
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-01-15 11:26:50 -0300

    Adicionando Swagger para documentar a API

--- src/main/java/com/mikaelsonbraz/bookstoreproject/config/SwaggerConfig.java
+++ src/main/java/com/mikaelsonbraz/bookstoreproject/config/SwaggerConfig.java
@@ -1,7 +1,44 @@
 package com.mikaelsonbraz.bookstoreproject.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+
+    private static final String BASE_PACKAGE = "com.mikaelsonbraz.bookstoreproject";
+    private static final String API_TITLE = "Bookstore Manager Course";
+    private static final String API_DESCRIPTION = "Bookstore Manager Professional";
+    private static final String API_VERSION = "1.0.0";
+    private static final String CONTACT_NAME = "Mikaelson Braz";
+    private static final String CONTACT_LINKEDINN = "https://www.linkedin.com/in/mikaelsonbraz/";
+    private static final String CONTACT_EMAIL = "mikaelsonmrn@gmail.com";
+
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build().apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION)
+                .contact(new Contact(CONTACT_NAME, CONTACT_LINKEDINN, CONTACT_EMAIL))
+                .build();
+    }
 }
