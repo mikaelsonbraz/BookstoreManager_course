e9b7e0911a5c0b2872c59a51eff78e487f262ea5
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-01-27 10:07:55 -0300

    created author DTO class

---
+++ src/main/java/com/mikaelsonbraz/bookstoreproject/author/dto/AuthorDTO.java
@@ -0,0 +1,26 @@
+package com.mikaelsonbraz.bookstoreproject.author.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+public class AuthorDTO {
+
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Size(max = 255)
+    private String name;
+
+    @NotNull
+    @Max(130)
+    private Integer age;
+}
